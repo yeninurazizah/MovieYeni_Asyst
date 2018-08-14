433b14eb38d0347b32ec56048a56416896460c5e
yeninurazizah <yeninurazizah14@gmail.com>
2018-08-13 17:01:59 -0700

    Mulai - Movie

---
+++ app/src/main/java/id/co/asyst/yeni/movieyeni/MainActivity.java
@@ -0,0 +1,19 @@
+package id.co.asyst.yeni.movieyeni;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recylerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
+//https://image.tmdb.org/t/p/w185_and_h278_bestv2
